c6740688161e988c2241761d9d414eec3109ad80
genusistimelord <genusistimelord@outlook.com>
2026-03-07 16:32:09 -0500

    optimized the Vecs and removed clipping as it not needed will try to do it a different better way

--- src/images/render.rs
+++ src/images/render.rs
@@ -1,6 +1,6 @@
 use crate::{
     AtlasSet, GpuRenderer, GraphicsError, Image, ImageRenderPipeline,
-    ImageVertex, InstanceBuffer, OrderedIndex, StaticVertexBuffer, System,
+    ImageVertex, InstanceBuffer, OrderedIndex, StaticVertexBuffer,
 };
 
 /// Instance Buffer Setup for [`Image`].
@@ -15,7 +15,7 @@ impl ImageRenderer {
     ///
     pub fn new(renderer: &GpuRenderer) -> Result<Self, GraphicsError> {
         Ok(Self {
-            buffer: InstanceBuffer::new(renderer.gpu_device(), 512),
+            buffer: InstanceBuffer::with_capacity(renderer.gpu_device(), 512),
         })
     }
 
@@ -61,20 +61,12 @@ impl ImageRenderer {
 
         self.add_buffer_store(renderer, index, buffer_layer);
     }
-
-    /// Sets the Instance Buffer to enable Rendering With Scissor Clipping.
-    /// This must be Set for the Optional Bounds to be used.
-    ///
-    pub fn use_clipping(&mut self) {
-        self.buffer.set_as_clipped();
-    }
 }
 
 /// Trait used to Grant Direct [`Image`] Rendering to [`wgpu::RenderPass`]
-pub trait RenderImage<'a, 'b, Controls>
+pub trait RenderImage<'a, 'b>
 where
     'b: 'a,
-    Controls: camera::controls::Controls,
 {
     /// Renders the all [`Image`]'s within the buffer layer to screen that have been processed and finalized.
     ///
@@ -83,70 +75,22 @@ where
         renderer: &'b GpuRenderer,
         buffer: &'b ImageRenderer,
         atlas: &'b AtlasSet,
-        system: &'b System<Controls>,
         buffer_layer: usize,
     );
 }
 
-impl<'a, 'b, Controls> RenderImage<'a, 'b, Controls> for wgpu::RenderPass<'a>
+impl<'a, 'b> RenderImage<'a, 'b> for wgpu::RenderPass<'a>
 where
     'b: 'a,
-    Controls: camera::controls::Controls,
 {
     fn render_image(
         &mut self,
         renderer: &'b GpuRenderer,
         buffer: &'b ImageRenderer,
         atlas: &'b AtlasSet,
-        system: &'b System<Controls>,
         buffer_layer: usize,
     ) {
-        if buffer.buffer.is_clipped() {
-            if let Some(details) =
-                buffer.buffer.clipped_buffers.get(buffer_layer)
-            {
-                let mut scissor_is_default = true;
-
-                if buffer.buffer.count() > 0 {
-                    self.set_bind_group(1, atlas.bind_group(), &[]);
-                    self.set_vertex_buffer(1, buffer.buffer.instances(None));
-                    self.set_pipeline(
-                        renderer.get_pipelines(ImageRenderPipeline).unwrap(),
-                    );
-                    for (details, bounds, camera_view) in details {
-                        if let Some(bounds) = bounds {
-                            let bounds =
-                                system.world_to_screen(*camera_view, bounds);
-
-                            self.set_scissor_rect(
-                                bounds.x as u32,
-                                bounds.y as u32,
-                                bounds.z as u32,
-                                bounds.w as u32,
-                            );
-                            scissor_is_default = false;
-                        }
-
-                        self.draw_indexed(
-                            0..StaticVertexBuffer::index_count(),
-                            0,
-                            details.start..details.end,
-                        );
-
-                        if !scissor_is_default {
-                            self.set_scissor_rect(
-                                0,
-                                0,
-                                system.screen_size[0] as u32,
-                                system.screen_size[1] as u32,
-                            );
-                            scissor_is_default = true;
-                        };
-                    }
-                }
-            }
-        } else if let Some(Some(details)) =
-            buffer.buffer.buffers.get(buffer_layer)
+        if let Some(Some(details)) = buffer.buffer.buffers.get(buffer_layer)
             && buffer.buffer.count() > 0
         {
             self.set_bind_group(1, atlas.bind_group(), &[]);
--- src/ui/render.rs
+++ src/ui/render.rs
@@ -1,6 +1,6 @@
 use crate::{
     AtlasSet, GpuRenderer, GraphicsError, InstanceBuffer, OrderedIndex, Rect,
-    RectRenderPipeline, RectVertex, StaticVertexBuffer, System,
+    RectRenderPipeline, RectVertex, StaticVertexBuffer,
 };
 
 /// Instance Buffer Setup for [`Rect`]'s.
@@ -16,7 +16,7 @@ impl RectRenderer {
     ///
     pub fn new(renderer: &GpuRenderer) -> Result<Self, GraphicsError> {
         Ok(Self {
-            buffer: InstanceBuffer::new(renderer.gpu_device(), 512),
+            buffer: InstanceBuffer::with_capacity(renderer.gpu_device(), 512),
         })
     }
 
@@ -62,20 +62,12 @@ impl RectRenderer {
 
         self.add_buffer_store(renderer, index, buffer_layer);
     }
-
-    /// Sets the Instance Buffer to enable Rendering With Scissor Clipping.
-    /// This must be Set for the Optional Bounds to be used.
-    ///
-    pub fn use_clipping(&mut self) {
-        self.buffer.set_as_clipped();
-    }
 }
 
 /// Trait used to Grant Direct [`Rect`] Rendering to [`wgpu::RenderPass`]
-pub trait RenderRects<'a, 'b, Controls>
+pub trait RenderRects<'a, 'b>
 where
     'b: 'a,
-    Controls: camera::controls::Controls,
 {
     /// Renders the all [`Rect`]'s within the buffer layer to screen that have been processed and finalized.
     ///
@@ -84,75 +76,22 @@ where
         renderer: &'b GpuRenderer,
         buffer: &'b RectRenderer,
         atlas: &'b AtlasSet,
-        system: &'b System<Controls>,
         buffer_layer: usize,
     );
 }
 
-impl<'a, 'b, Controls> RenderRects<'a, 'b, Controls> for wgpu::RenderPass<'a>
+impl<'a, 'b> RenderRects<'a, 'b> for wgpu::RenderPass<'a>
 where
     'b: 'a,
-    Controls: camera::controls::Controls,
 {
     fn render_rects(
         &mut self,
         renderer: &'b GpuRenderer,
         buffer: &'b RectRenderer,
         atlas: &'b AtlasSet,
-        system: &'b System<Controls>,
         buffer_layer: usize,
     ) {
-        if buffer.buffer.is_clipped() {
-            if let Some(details) =
-                buffer.buffer.clipped_buffers.get(buffer_layer)
-            {
-                let mut scissor_is_default = true;
-
-                if buffer.buffer.count() > 0 {
-                    self.set_bind_group(
-                        1,
-                        &atlas.texture_group.bind_group,
-                        &[],
-                    );
-                    self.set_vertex_buffer(1, buffer.buffer.instances(None));
-                    self.set_pipeline(
-                        renderer.get_pipelines(RectRenderPipeline).unwrap(),
-                    );
-
-                    for (details, bounds, camera_view) in details {
-                        if let Some(bounds) = bounds {
-                            let bounds =
-                                system.world_to_screen(*camera_view, bounds);
-
-                            self.set_scissor_rect(
-                                bounds.x as u32,
-                                bounds.y as u32,
-                                bounds.z as u32,
-                                bounds.w as u32,
-                            );
-                            scissor_is_default = false;
-                        }
-
-                        self.draw_indexed(
-                            0..StaticVertexBuffer::index_count(),
-                            0,
-                            details.start..details.end,
-                        );
-
-                        if !scissor_is_default {
-                            self.set_scissor_rect(
-                                0,
-                                0,
-                                system.screen_size[0] as u32,
-                                system.screen_size[1] as u32,
-                            );
-                            scissor_is_default = true;
-                        };
-                    }
-                }
-            }
-        } else if let Some(Some(details)) =
-            buffer.buffer.buffers.get(buffer_layer)
+        if let Some(Some(details)) = buffer.buffer.buffers.get(buffer_layer)
             && buffer.buffer.count() > 0
         {
             self.set_bind_group(1, &atlas.texture_group.bind_group, &[]);
